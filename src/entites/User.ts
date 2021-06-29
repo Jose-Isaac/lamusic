214c417a4a05818b2f8ba9519f403349f0a42be9
José Isaac <joseisaacbarreirocampos@gmail.com>
2021-06-29 14:47:48 -0300

    Adicionado a entidade user

---
+++ src/entites/User.ts
@@ -0,0 +1,38 @@
+export class User {
+  constructor(
+    private readonly id: string,
+    private name: string,
+    private nickname: string,
+    private email: string,
+    private password: string
+  ) {}
+
+  public getId(): string {
+    return this.id;
+  }
+  public getName(): string {
+    return this.name;
+  }
+  public getNickname(): string {
+    return this.nickname;
+  }
+  public getEmail(): string {
+    return this.email;
+  }
+  public getPassword(): string {
+    return this.password;
+  }
+
+  public setName(name: string): void {
+    this.name = name;
+  }
+  public setNickname(nickname: string): void {
+    this.nickname = nickname;
+  }
+  public setEmail(email: string): void {
+    this.email = email;
+  }
+  public setPassword(password: string): void {
+    this.password = password;
+  }
+}
