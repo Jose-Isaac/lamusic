9c8e8aa864923c839a8eb71614532ab9de79e1b6
José Isaac <joseisaacbarreirocampos@gmail.com>
2021-06-30 14:09:34 -0300

    Adicionado validação para a senha de no minimo 6 caracteres

--- src/business/UserBusiness.ts
+++ src/business/UserBusiness.ts
@@ -4,40 +4,40 @@ import { ConflictError } from '../error/ConflictError';
 import { MissingDependenciesError } from '../error/MissingDependenciesError';
 import { HashManager } from '../services/HashManager';
 import { IdGenerator } from '../services/IdGenerator';
+import { PasswordFormatValidator } from '../services/PasswordFormatValidator';
 import { userInputDTO } from '../types/user';
 
 export class UserBusiness {
   public async signup(input: userInputDTO) {
-    try {
-      const { name, nickname, email, password } = input;
+    const { name, nickname, email, password } = input;
 
-      if (!name || !nickname || !email || !password) {
-        throw new MissingDependenciesError(
-          'Missing dependencies: "name" or "nickname" or "email" or "password"'
-        );
-      }
-
-      // TODO validar email
-
-      // TODO validar password
-
-      const userDatabase = new UserDatabase();
-      const thisEmailExist = await userDatabase.getByEmail(email);
-
-      if (thisEmailExist) {
-        throw new ConflictError('Sorry, email already registered');
-      }
-
-      const idGenerator = new IdGenerator();
-      const id = idGenerator.generate();
-
-      const hashManager = new HashManager();
-      const hashPassword = await hashManager.hash(password);
-
-      const user = new User(id, name, nickname, email, hashPassword);
-      await userDatabase.create(user);
-    } catch (error) {
-      throw new Error(error.sqlMessage || error.message);
+    if (!name || !nickname || !email || !password) {
+      throw new MissingDependenciesError(
+        'Missing dependencies: "name" or "nickname" or "email" or "password"'
+      );
     }
+
+    // TODO validar email
+
+    const passwordFormatValidator = new PasswordFormatValidator();
+
+    passwordFormatValidator.validate(password);
+    // TODO validar password
+
+    const userDatabase = new UserDatabase();
+    const thisEmailExist = await userDatabase.getByEmail(email);
+
+    if (thisEmailExist) {
+      throw new ConflictError('Sorry, email already registered');
+    }
+
+    const idGenerator = new IdGenerator();
+    const id = idGenerator.generate();
+
+    const hashManager = new HashManager();
+    const hashPassword = await hashManager.hash(password);
+
+    const user = new User(id, name, nickname, email, hashPassword);
+    await userDatabase.create(user);
   }
 }
