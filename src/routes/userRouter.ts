3b18952e4bc3fcc37a713972fce6bdd593c218dc
José Isaac <joseisaacbarreirocampos@gmail.com>
2021-06-30 15:49:09 -0300

    Adicionado a rota de login

--- src/routes/userRouter.ts
+++ src/routes/userRouter.ts
@@ -6,5 +6,6 @@ const userRouter = express.Router();
 const userController = new UserController();
 
 userRouter.post('/signup', userController.signup);
+userRouter.post('/login', userController.login);
 
 export { userRouter };
