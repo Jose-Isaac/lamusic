d5e938cd446959f43b03452dddd54912cf12b7f7
José Isaac <joseisaacbarreirocampos@gmail.com>
2021-07-02 08:48:48 -0300

    Ajuste no tratamento de erros

--- src/business/UserBusiness.ts
+++ src/business/UserBusiness.ts
@@ -1,5 +1,6 @@
 import { UserDatabase } from '../data/UserDatabase';
 import { User } from '../entities/User';
+import { BaseError } from '../error/BaseError';
 import { ConflictError } from '../error/ConflictError';
 import { InvalidCredentialsError } from '../error/InvalidCredentialsError';
 import { MissingDependenciesError } from '../error/MissingDependenciesError';
@@ -13,87 +14,95 @@ import { userAuthenticatorCredentials, userInputDTO } from '../types/user';
 
 export class UserBusiness {
   public async signup(input: userInputDTO): Promise<string> {
-    const { name, nickname, email, password } = input;
+    try {
+      const { name, nickname, email, password } = input;
 
-    if (!name || !nickname || !email || !password) {
-      throw new MissingDependenciesError(
-        'Missing dependencies: "name" or "nickname" or "email" or "password"'
+      if (!name || !nickname || !email || !password) {
+        throw new MissingDependenciesError(
+          'Missing dependencies: "name" or "nickname" or "email" or "password"'
+        );
+      }
+
+      const emailFormatValidator = new EmailFormatValidator();
+      emailFormatValidator.validate(email);
+
+      const passwordFormatValidator = new PasswordFormatValidator();
+      passwordFormatValidator.validate(password);
+
+      const userDatabase = new UserDatabase();
+      const thisEmailExist = await userDatabase.getByEmail(email);
+
+      if (thisEmailExist) {
+        throw new ConflictError('Sorry, email already registered');
+      }
+
+      const idGenerator = new IdGenerator();
+      const extern_id = idGenerator.generate();
+
+      const hashManager = new HashManager();
+      const hashPassword = await hashManager.hash(password);
+
+      const userForDatabase = new User(
+        extern_id,
+        name,
+        nickname,
+        email,
+        hashPassword
       );
+      const user = await userDatabase.create(userForDatabase);
+
+      const authenticator = new Authenticator();
+      const token = authenticator.generateToken({
+        id: user.getExternId(),
+        role: user.getRole(),
+      });
+
+      return token;
+    } catch (error) {
+      throw new BaseError(error.sqlMessage || error.message, error.code || 500);
     }
-
-    const emailFormatValidator = new EmailFormatValidator();
-    emailFormatValidator.validate(email);
-
-    const passwordFormatValidator = new PasswordFormatValidator();
-    passwordFormatValidator.validate(password);
-
-    const userDatabase = new UserDatabase();
-    const thisEmailExist = await userDatabase.getByEmail(email);
-
-    if (thisEmailExist) {
-      throw new ConflictError('Sorry, email already registered');
-    }
-
-    const idGenerator = new IdGenerator();
-    const extern_id = idGenerator.generate();
-
-    const hashManager = new HashManager();
-    const hashPassword = await hashManager.hash(password);
-
-    const userForDatabase = new User(
-      extern_id,
-      name,
-      nickname,
-      email,
-      hashPassword
-    );
-    const user = await userDatabase.create(userForDatabase);
-
-    const authenticator = new Authenticator();
-    const token = authenticator.generateToken({
-      id: user.getExternId(),
-      role: user.getRole(),
-    });
-
-    return token;
   }
 
   public async login(input: userAuthenticatorCredentials): Promise<string> {
-    const { email, password } = input;
+    try {
+      const { email, password } = input;
 
-    // TODO aceitar nickname como forma de login
-    if (!email || !password) {
-      throw new MissingDependenciesError(
-        'Missing dependencies: "email" and "password"'
-      );
+      // TODO aceitar nickname como forma de login
+      if (!email || !password) {
+        throw new MissingDependenciesError(
+          'Missing dependencies: "email" and "password"'
+        );
+      }
+
+      const emailFormatValidator = new EmailFormatValidator();
+      emailFormatValidator.validate(email);
+
+      const passwordFormatValidator = new PasswordFormatValidator();
+      passwordFormatValidator.validate(password);
+
+      const userDatabase = new UserDatabase();
+      const user = await userDatabase.getByEmail(email);
+
+      if (!user) {
+        throw new NotFoundError('User not found');
+      }
+
+      const hashManager = new HashManager();
+      const passwordMatch = hashManager.compare(password, user.getPassword());
+
+      if (!passwordMatch) {
+        throw new InvalidCredentialsError('Invalid password');
+      }
+
+      const authenticator = new Authenticator();
+      const token = authenticator.generateToken({
+        id: user.getExternId(),
+        role: user.getRole(),
+      });
+
+      return token;
+    } catch (error) {
+      throw new BaseError(error.sqlMessage || error.message, error.code || 500);
     }
-
-    const emailFormatValidator = new EmailFormatValidator();
-    emailFormatValidator.validate(email);
-
-    const passwordFormatValidator = new PasswordFormatValidator();
-    passwordFormatValidator.validate(password);
-
-    const userDatabase = new UserDatabase();
-    const user = await userDatabase.getByEmail(email);
-
-    if (!user) {
-      throw new NotFoundError('User not found');
-    }
-
-    const hashManager = new HashManager();
-    const passwordMatch = hashManager.compare(password, user.getPassword());
-
-    if (!passwordMatch) {
-      throw new InvalidCredentialsError('Invalid password');
-    }
-
-    const authenticator = new Authenticator();
-    const token = authenticator.generateToken({
-      id: user.getExternId(),
-      role: user.getRole(),
-    });
-
-    return token;
   }
 }
